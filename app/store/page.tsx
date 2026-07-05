adbbd80be7a6642c333f3f64d0cbef959b5ef820
Sage Smith <sagemistressmods@gmail.com>
2026-07-05 20:00:22 -0300

    expand store iframe width

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -3,7 +3,7 @@ export default function Store() {
         <iframe 
             className="m-auto mt-4" 
             src="https://nottalabel.square.site/s/shop" 
-            width="80%" 
+            width="90%" 
             height="800px" 
         />
         <p>If the window above is not loading, please visit <a className="underline" href="https://nottalabel.square.site">this link</a> instead.</p>
